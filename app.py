01c55184c2b5770607f2b029b808a4adf9b17427
Lee Liang <52850586+lnl1119@users.noreply.github.com>
2024-10-13 10:42:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 #!pip install langchain langchain-community langchain-openai openai tiktoken chromadb langchain_chroma streamlit
-
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 from langchain_chroma import Chroma
 from langchain.prompts.chat import (
